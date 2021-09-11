0b1c2c5edadaaddbfdebe2150cc1155f306de55d
Norber <nox_nox_@hotmail.com>
2021-09-11 11:20:49 +0100

    Login screen. KeyboardAwareScrollView installed

---
+++ screens/Login.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Image } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Login() {
+  return (
+    <SafeAreaView>
+      <KeyboardAwareScrollView>
+
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  )
+}
